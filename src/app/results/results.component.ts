058085854c6a9863263b6a0a7a9aa1f1e3830a53
Alex <50662248+alxattxr@users.noreply.github.com>
2020-08-31 19:07:21 -0400

    add- basic styling to the results component and add mock data

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 'results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.scss']
+  styleUrls: ['./results.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ResultsComponent implements OnInit {
+export class ResultsComponent {
+  @Input() searchedString: string;
 
+  list = [1,2,2,2,2,2,2,2,2]
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
