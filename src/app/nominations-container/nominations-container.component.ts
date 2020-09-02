94bcf37c8502c5f4ac3195fc018fb729ea5104af
Alex <50662248+alxattxr@users.noreply.github.com>
2020-09-01 20:08:24 -0400

    add - nomination store logic

--- src/app/nominations-container/nominations-container.component.ts
+++ src/app/nominations-container/nominations-container.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SearchMovieStoreService } from '../services/search-movie-store.service';
+import { MovieInformation } from '../models/MovieInformation.model';
 
 @Component({
   selector: 'nominations-container',
   templateUrl: './nominations-container.component.html',
-  styleUrls: ['./nominations-container.component.scss']
+  styleUrls: ['./nominations-container.component.scss'],
 })
-export class NominationsContainerComponent implements OnInit {
+export class NominationsContainerComponent {
+  public searchMovieStoreService: SearchMovieStoreService;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(searchMovieStoreService: SearchMovieStoreService) {
+    this.searchMovieStoreService = searchMovieStoreService;
   }
 
+  public removeFronNominations(nominee: MovieInformation): void {
+    console.log(nominee);
+    this.searchMovieStoreService.removeElementFromNominations(nominee);
+    console.log('current nominations: ', this.searchMovieStoreService.nominations);
+  }
 }
--- src/app/services/search-movie-store.service.ts
+++ src/app/services/search-movie-store.service.ts
@@ -1,45 +1,62 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
-import { SearchResults } from "../models/SearchResults.model";
-import { MovieInformation } from "../models/MovieInformation.model";
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { SearchResults } from '../models/SearchResults.model';
+import { MovieInformation } from '../models/MovieInformation.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn : 'root',
 })
 export class SearchMovieStoreService {
-    readonly _searchedString = new BehaviorSubject<string>('');
-    readonly searchedString$ = this._searchedString.asObservable();
-    readonly _isLoading = new BehaviorSubject<boolean>(false);
-    readonly isLoading$ = this._isLoading.asObservable();
-    readonly _results = new BehaviorSubject<SearchResults>({
-        Response: false,
-        Search: [],
-        totalResults: "0"
-    });
-    readonly results$ = this._results.asObservable();
+  readonly _searchedString = new BehaviorSubject<string>('');
+  readonly searchedString$ = this._searchedString.asObservable();
+  readonly _isLoading = new BehaviorSubject<boolean>(false);
+  readonly isLoading$ = this._isLoading.asObservable();
+  readonly _results = new BehaviorSubject<SearchResults>(null);
+  readonly results$ = this._results.asObservable();
 
-    get searchedString(): string {
-        return this._searchedString.getValue();
-    }
+  readonly _nominations = new BehaviorSubject<MovieInformation[]>([]);
+  readonly nominations$ = this._results.asObservable();
 
-    set searchedString(str: string) {
-        this._searchedString.next(str);
-    }
+  get searchedString(): string {
+    return this._searchedString.getValue();
+  }
 
-    get results(): SearchResults {
-        return this._results.getValue();
-    }
+  set searchedString(str: string) {
+    this._searchedString.next(str);
+  }
 
-    set results(results: SearchResults) {
-        this._results.next(results);
-    }
+  get results(): SearchResults {
+    return this._results.getValue();
+  }
 
-    getSearchResults(): MovieInformation[] {
-        return this._results.getValue().Search;
-    }
+  set results(results: SearchResults) {
+    this._results.next(results);
+  }
 
-    getSearchResponse(): boolean {
-        return this._results.getValue().Response;
-    }
+  get nominations(): MovieInformation[] {
+    return this._nominations.getValue();
+  }
 
+  set nominations(nominations: MovieInformation[]) {
+    this._nominations.next(nominations);
+  }
+
+  //Nominations functions
+  public addElementToNominations(movie: MovieInformation): void {
+    this._nominations.getValue().push(movie);
+  }
+
+  public removeElementFromNominations(movie: MovieInformation): void {
+    console.log(this._nominations.getValue().filter((m) => m !== movie));
+    this.nominations = this._nominations.getValue().filter((m) => m !== movie);
+  }
+
+  //Results functions
+  getSearchResults(): MovieInformation[] {
+    return this._results.getValue().Search;
+  }
+
+  getSearchResponse(): boolean {
+    return this._results.getValue().Response;
+  }
 }
