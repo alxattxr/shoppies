10baab2d8117917c478c0fdc48c03926241826df
Alexandre Attar <hello@alxndrattxr.com>
2020-09-03 16:41:26 -0400

    fix - potential memory leak

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,8 +1,9 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { SearchMovieStoreService } from '../services/search-movie-store.service';
 import { SearchService } from '../services/search-movies.service';
 import { SearchResults } from '../models/SearchResults.model';
 import { ResultType } from '../enums/ResultType.enum';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'search-bar',
@@ -10,15 +11,20 @@ import { ResultType } from '../enums/ResultType.enum';
   styleUrls: ['./search-bar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnDestroy {
+  private _subscription: Subscription;
+  public searchedTerm = '';
+  //Since we only want movies
+  private searchType: ResultType = ResultType.Movie;
+
   constructor(
     private _searchMovieStoreService: SearchMovieStoreService,
     private _searchService: SearchService
   ) { }
 
-  public searchedTerm = '';
-  //Since we only want movies
-  private searchType: ResultType = ResultType.Movie;
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
+  }
 
   public eraseEntry(): void {
     this.searchedTerm = '';
@@ -27,7 +33,7 @@ export class SearchBarComponent {
   public onSubmit(): void {
     this._searchMovieStoreService.searchedString = this.searchedTerm;
     this._searchMovieStoreService.isLoading = true;
-    this._searchService
+    this._subscription = this._searchService
       .getMoviesInformations(this.searchedTerm, this.searchType)
       .subscribe((searchResult: SearchResults) => {
         this._searchMovieStoreService.results = searchResult;
