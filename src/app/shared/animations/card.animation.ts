c29eea33a685ffc42542630c1f58b95b1ea72f79
Alex <50662248+alxattxr@users.noreply.github.com>
2020-09-13 13:45:58 -0400

    fix - remove unuseful comment

--- src/app/shared/animations/card.animation.ts
+++ src/app/shared/animations/card.animation.ts
@@ -2,9 +2,9 @@ import { trigger, transition, style, animate } from '@angular/animations';
 
 export const CardAnimation = trigger('items', [
   transition(':enter', [
-    style({ transform: 'scale(0.8)', opacity: 0 }),  // initial
+    style({ transform: 'scale(0.8)', opacity: 0 }),
     animate('0.5s cubic-bezier(.8, -0.6, 0.2, 1.5)',
-      style({ transform: 'scale(1)', opacity: 1 }))  // final
+      style({ transform: 'scale(1)', opacity: 1 }))
   ]),
   transition(':leave', [
     style({ transform: 'scale(1)', opacity: 1, height: '*' }),
