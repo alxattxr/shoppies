8d6cfc9d49ace3863d53c8ab8cf1533297a1e6ac
Alexandre Attar <hello@alxndrattxr.com>
2021-01-10 20:40:04 -0500

    nomination place holder

---
+++ src/app/nomination-placeholder/nomination-placeholder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nomination-placeholder',
+  templateUrl: './nomination-placeholder.component.html',
+  styleUrls: ['./nomination-placeholder.component.scss']
+})
+export class NominationPlaceholderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
